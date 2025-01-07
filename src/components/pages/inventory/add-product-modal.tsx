3c86bf9ec6bfb71fcfd2b427b93055eb0df3c888
Samson Mesioye <jazzygee36gmail.com>
2025-01-07 23:39:22 +0100

    add product

--- src/components/pages/inventory/add-product-modal.tsx
+++ src/components/pages/inventory/add-product-modal.tsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import HomeButton from '@/components/common/button';
+import HomeInput from '@/components/common/input';
+import { addProductSchema } from '@/components/utils/validation';
+import React, { useState } from 'react';
+import { z } from 'zod';
+
+type FormData = z.infer<typeof addProductSchema>;
 
 const AllProductModal = () => {
-  return <div>Add Product</div>;
+  const [data, setData] = useState<FormData>({
+    productName: '',
+    buyingPrice: '',
+    qtyBought: '',
+    salesPrice: '',
+    qtySold: '',
+    expired: '',
+    availability: '',
+  });
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const handleAddProduct = (e: React.FormEvent) => {
+    e.preventDefault();
+    const result = addProductSchema.safeParse(data);
+    if (!result.success) {
+      const validationErrors = result.error.format();
+      setErrors({
+        productName: validationErrors.productName?._errors[0] || '',
+        buyingPrice: validationErrors.buyingPrice?._errors[0] || '',
+        qtyBought: validationErrors.qtyBought?._errors[0] || '',
+        salesPrice: validationErrors.salesPrice?._errors[0] || '',
+        qtySold: validationErrors.qtySold?._errors[0] || '',
+        expired: validationErrors.expired?._errors[0] || '',
+        availability: validationErrors.availability?._errors[0] || '',
+      });
+      return;
+    }
+  };
+  return (
+    <div className=''>
+      <form onSubmit={handleAddProduct}>
+        <HomeInput placeholder={'PRODUCT NAME'} />
+        {errors.productName && (
+          <p className='text-red-500 text-[13px]'>{errors.productName}</p>
+        )}
+        <HomeInput placeholder={'BUYING PRICE'} />
+        <HomeInput placeholder={'QTY BOUGHT'} />
+        <HomeInput placeholder={'SALES PRICE'} />
+        <HomeInput placeholder={'QTY SOLD'} />
+        <HomeInput placeholder={'EXPIRED'} />
+        <HomeInput placeholder={'AVAILABILITY'} />
+        <div className='mt-3'>
+          <HomeButton
+            title={'Add Product'}
+            bg={'#2E5BFF'}
+            color={'white'}
+            type='submit'
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AllProductModal;
--- src/components/utils/validation.ts
+++ src/components/utils/validation.ts
@@ -23,3 +23,13 @@ export const staffLoginSchema = z.object({
   username: z.string().min(3, 'Username is required'),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
+
+export const addProductSchema = z.object({
+  productName: z.string().min(3, 'Product is required'),
+  buyingPrice: z.string().min(2, 'Buying price is required'),
+  qtyBought: z.string().min(1, 'Quantity bought is required'),
+  salesPrice: z.string().min(1, 'Selling price is required'),
+  qtySold: z.string().min(1, 'Quantity sold is required'),
+  expired: z.string().min(1, 'Expired date is required'),
+  availability: z.string().min(3, 'Availability is required'),
+});
