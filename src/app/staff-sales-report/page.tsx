8f5f9d57bd4b7056c504f1317e51313981feb0f3
Samson Mesioye <jazzygee36gmail.com>
2025-01-16 22:47:11 +0100

    sales report

--- src/app/staff-sales-report/page.tsx
+++ src/app/staff-sales-report/page.tsx
@@ -14,3 +14,10 @@ const DailySalesReport = () => {
 };
 
 export default DailySalesReport;
+
+// This forces server-side rendering
+export async function getServerSideProps() {
+  return {
+    props: {}, // Pass props if needed
+  };
+}
