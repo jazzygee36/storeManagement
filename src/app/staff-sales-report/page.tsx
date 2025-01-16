e9cd34fa0f055fe3965e9bb4fe588d31ab2750ff
Samson Mesioye <jazzygee36gmail.com>
2025-01-16 22:29:05 +0100

    sales report

--- src/app/staff-sales-report/page.tsx
+++ src/app/staff-sales-report/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+import Loading from '@/components/common/loadingState';
 import MainStaffDashboard from '@/components/common/staff-dashboard/main-staff-dashboard';
+import useAuth from '@/components/hook/useAuth';
 import React from 'react';
 
 const DailySalesReport = () => {
-  return <MainStaffDashboard>DailySalesReport</MainStaffDashboard>;
+  const isAuthenticated = useAuth();
+  return (
+    <MainStaffDashboard>
+      {!isAuthenticated ? <Loading /> : <h1>Staff report</h1>}
+    </MainStaffDashboard>
+  );
 };
 
 export default DailySalesReport;
