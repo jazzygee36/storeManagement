c4d537dd39526d0a27bb0cc64928e6b4a6b5226e
Samson Mesioye <jazzygee36gmail.com>
2025-01-16 19:54:23 +0100

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,4 @@
+// src/app/register/page.tsx
 'use client';
 
 import HomeInput from '@/components/common/input';
@@ -21,10 +22,7 @@ import Loading from '@/components/common/loadingState';
 
 type FormData = z.infer<typeof registerSchema>;
 
-function RegisterForm({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<'div'>) {
+export default function RegisterPage() {
   const dispatch = useDispatch();
   const router = useRouter();
   const [data, setData] = useState<FormData>({
@@ -34,11 +32,12 @@ function RegisterForm({
   });
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  console.log('errors', errors);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setData((prevData) => ({ ...prevData, [name]: value }));
   };
+
   const handleRegisterSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -53,20 +52,17 @@ function RegisterForm({
       });
       return; // Exit if validation fails
     }
+
     setLoading(true);
     setErrors({}); // Clear errors before submission
 
     try {
-      setLoading(true);
-
       const res = await axios.post(
         `${process.env.NEXT_PUBLIC_BASE_URL}/register`,
         data
       );
       dispatch(setUserSignUp(res.data)); // Update Redux store
-
       router.push('/'); // Redirect on success
-      setLoading(false);
     } catch (err) {
       if (axios.isAxiosError(err)) {
         if (err.response?.data?.message) {
@@ -85,13 +81,12 @@ function RegisterForm({
       setLoading(false);
     }
   };
+
   return (
     <div
       className={cn(
-        'h-screen flex flex-col gap-6 m-auto w-full items-center justify-center',
-        className
+        'h-screen flex flex-col gap-6 m-auto w-full items-center justify-center'
       )}
-      {...props}
     >
       <Card>
         <CardHeader>
@@ -99,13 +94,13 @@ function RegisterForm({
             Create an Account
           </CardTitle>
           <CardDescription>
-            Welcome to StockTaker! we give you control over your business
+            Welcome to StockTaker! We give you control over your business.
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleRegisterSubmit}>
             {errors.general && (
-              <p className='text-red-500 text-[13px] text-center '>
+              <p className='text-red-500 text-[13px] text-center'>
                 {errors.general}
               </p>
             )}
@@ -115,7 +110,6 @@ function RegisterForm({
                   label='Username'
                   type='text'
                   placeholder='Username'
-                  // required
                   value={data.username}
                   name='username'
                   onChange={handleChange}
@@ -150,16 +144,15 @@ function RegisterForm({
                   <p className='text-red-500 text-[13px]'>{errors.password}</p>
                 )}
               </div>
-
               <HomeButton
                 type='submit'
                 title={loading ? <Loading /> : 'Sign up'}
-                color={'white'}
+                color='white'
               />
             </div>
           </form>
           <div className='mt-4 text-center text-sm w-full'>
-            Alredy have an account?{' '}
+            Already have an account?{' '}
             <span
               className='underline underline-offset-4 cursor-pointer text-blue-600'
               onClick={() => router.push('/')}
@@ -170,82 +163,5 @@ function RegisterForm({
         </CardContent>
       </Card>
     </div>
-    // <div className='hidden md:block m-auto'>
-    //   {/* <Image src={Logo} alt='logo' /> */}
-    //   Welcome to StockTaker
-    // </div>
-    /* <div>
-          <div className='mb-[24px] flex justify-center'>
-            <Image src={SmallLogo} alt='logo' />
-          </div>
-          <h2 className='font-semibold text-[30px] text-[#101828] font-inter text-center'>
-            Create an Account
-          </h2>
-          <p className='text-[#667085] text-[16px] mt-3 mb-8 text-center'>
-            Star your 30-days free trial
-          </p>
-          <form
-            onSubmit={handleRegisterSubmit}
-            className=' md:px-5 lg:px-[130px]'
-          >
-            <HomeInput
-              label={'Username'}
-              placeholder={'Enter your username'}
-              type={'text'}
-              name='username'
-              value={data.username}
-              onChange={handleChange}
-              mt='5'
-            />
-            {errors.username && (
-              <p className='text-red-500 text-[13px]'>{errors.username}</p>
-            )}
-            <HomeInput
-              label={'Email'}
-              placeholder={'Enter your email'}
-              type={'email'}
-              name='email'
-              value={data.email}
-              onChange={handleChange}
-              mt='5'
-            />
-            {errors.email && (
-              <p className='text-red-500 text-[13px]'>{errors.email}</p>
-            )}
-            <HomeInput
-              label={'Password'}
-              placeholder={'Enter your password'}
-              type={'password'}
-              name='password'
-              value={data.password}
-              onChange={handleChange}
-              mt='5'
-            />
-            {errors.password && (
-              <p className='text-red-500 text-[13px]'>{errors.password}</p>
-            )}
-            {/* <p className='text-[14px] text-[#D0D5DD]'>
-            Must be at least 8 characters.
-          </p> */
-
-    /* <p className='text-[14px] text-[#667085]  font-medium mb-5  cursor-pointer'>
-              Must be at least 8 characters.
-            </p>
-            {errors && (
-              <p className='text-center text-[red]'>{errors.general}</p>
-            )}
-            <HomeButton
-              title={loading ? 'LOADING...' : 'Get Started'}
-              type={'submit'}
-              color='white'
-              disabled={loading}
-            />
-          </form>
-          
-        </div> */
-
-    // </>
   );
 }
-
-export default RegisterForm;
